b191b470f8c379560081df18f431010af78ac020
himashuraj2004 <131295247+himashuraj2004@users.noreply.github.com>
2025-05-18 00:37:27 +0530

    Complete signup page

--- bank management system/src/bank/management/system/SignupTwo.java
+++ bank management system/src/bank/management/system/SignupTwo.java
@@ -186,6 +186,8 @@ public class SignupTwo extends JFrame implements ActionListener{
                 c.s.executeUpdate(query);
                 
                 //signup Object;
+                setVisible(false);
+                new SignupThree(formno).setVisible(true);
         } catch (Exception e) {
             System.out.println(e);
         }
