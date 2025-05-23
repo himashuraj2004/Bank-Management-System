c994b840c69e92351ee266917433d28ae73d3a4d
himashuraj2004 <131295247+himashuraj2004@users.noreply.github.com>
2025-05-23 17:43:44 +0530

    added login

--- bank management system/src/bank/management/system/Login.java
+++ bank management system/src/bank/management/system/Login.java
@@ -4,6 +4,7 @@ package bank.management.system;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
 
 public class Login extends JFrame implements ActionListener{
     
@@ -87,7 +88,22 @@ public class Login extends JFrame implements ActionListener{
             pinTextField.setText("");
         }
         else if(ae.getSource() == login){
-            
+            Conn conn = new Conn();
+            String cardNumber = cardTextField.getText();
+            String pinnumber = pinTextField.getText();
+            String query = "select * from login where cardNumber = '"+cardNumber+"' and pin = '"+pinnumber+"'";
+            try{
+                ResultSet rs = conn.s.executeQuery(query);
+                if(rs.next()){
+                    setVisible(false);
+                    new Transactions(pinnumber).setVisible(true);
+                }
+                else{
+                    JOptionPane.showMessageDialog(null,"Incorrect Card Number or Pin");
+                }
+            }catch(Exception e){
+             System.out.println(e);
+            }
         }
         else if(ae.getSource() == signup){
             setVisible(false);
---
+++ bank management system/src/bank/management/system/Transactions.java
@@ -0,0 +1,79 @@
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Transactions extends JFrame implements ActionListener{
+    JButton deposit, withdrawl, miniStatement, pinchange, fastCash, balance, exit; 
+    String pinnumber;
+    Transactions(String pinnumber){
+        this.pinnumber = pinnumber;
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 900, 900);
+        add(image);
+        
+        JLabel text = new JLabel("Please select your Transaction");
+        text.setBounds(218, 300, 700, 35);
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("System", Font.BOLD, 16));
+        image.add(text);
+        
+        deposit = new JButton("Deposit");
+        deposit.setBounds(170, 415, 150, 30);
+        deposit.addActionListener(this);
+        image.add(deposit);
+        
+        withdrawl = new JButton("Cash Withdrawl");
+        withdrawl.setBounds(355, 415, 150, 30);
+        withdrawl.addActionListener(this);
+        image.add(withdrawl);
+        
+        fastCash = new JButton("Fast Cash");
+        fastCash.setBounds(170, 450, 150, 30);
+        fastCash.addActionListener(this);
+        image.add(fastCash);
+        
+        miniStatement = new JButton("Mini Statement");
+        miniStatement.setBounds(355, 450, 150, 30);
+        miniStatement.addActionListener(this);
+        image.add(miniStatement);
+        
+        pinchange = new JButton("PIN Change");
+        pinchange.setBounds(170, 485, 150, 30);
+        pinchange.addActionListener(this);
+        image.add(pinchange);
+        
+        balance = new JButton("Balance Enquiry");
+        balance.setBounds(355, 485, 150, 30);
+        balance.addActionListener(this);
+        image.add(balance);
+        
+        exit = new JButton("EXIT");
+        exit.setBounds(170, 520, 150, 30);
+        exit.setBackground(Color.RED);
+        exit.addActionListener(this);
+        image.add(exit);
+        
+        
+        setSize(900, 900);
+        setLocation(300, 0);
+        setUndecorated(true);
+        setVisible(true);
+        
+    }
+    
+    public void actionPerformed(ActionEvent ae){
+        if(ae.getSource() == exit){
+        System.exit(0);
+        }
+    }
+    
+    public static void main(String args[]){
+        new Transactions("");
+    }
+}
